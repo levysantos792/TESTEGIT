838784a0c8ef43172896c5c07ca2a5763b4cf44f
levysantos792 <levysantos792@gmail.com>
2023-01-21 01:30:51 -0300

    atualizado

--- aulas/AULA1.py
+++ aulas/AULA1.py
@@ -3,12 +3,3 @@ print('qual é seu nome ?')
 nome= input()
 print('Então seu nome é '+nome)
 
-print('comando git status')
-a=5
-b=6
-c = 100
-print(a+b)
-if a+b>c:
-    print('verdadeiro')
-else:
-    print('false') 
