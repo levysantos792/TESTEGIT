cc946eadfaafb38d5d44711995a48ee0126b2618
levysantos792 <levysantos792@gmail.com>
2023-01-21 00:10:41 -0300

    add uma condicional

--- AULA2.py
+++ AULA2.py
@@ -2,3 +2,14 @@ print('comando git status')
 print('digite o seu nome')
 nome=input()
 print('seu nome é: '+nome)
+a=1
+b=3
+c=4
+if a+b>c:
+    print('verdadeiro')
+else:
+    print('false') 
+
+
+
+
