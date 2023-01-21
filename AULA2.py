e8126da27f33130cd8206b5f61be1066e1ab80dd
levysantos792 <levysantos792@gmail.com>
2023-01-21 00:26:39 -0300

    modifiquei aula 2

--- AULA2.py
+++ AULA2.py
@@ -1,14 +1,13 @@
 print('comando git status')
-print('digite o seu nome')
-nome=input()
-print('seu nome é: '+nome)
-a=1
-b=3
-c=4
+a=5
+b=6
+c = 100
+print(a+b)
 if a+b>c:
     print('verdadeiro')
 else:
     print('false') 
+
 
 
 
